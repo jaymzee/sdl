954f2ee77f6f6782ffb44f5591fcb88abb4ff83c
jaymzee <jaymzee@users.noreply.github.com>
2020-11-03 19:19:32 -0500

    draw loop

--- hello.c
+++ hello.c
@@ -9,7 +9,17 @@
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-int main(int argc, char* args[]) {
+void draw(SDL_Renderer *renderer, int frame)
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+    SDL_RenderDrawLine(renderer, frame, 0, 200 + frame, 200);
+    SDL_RenderPresent(renderer);
+}
+
+int main(int argc, char* args[])
+{
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
         return 1;
@@ -22,14 +32,13 @@ int main(int argc, char* args[]) {
         return 1;
     }
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-    SDL_RenderDrawLine(renderer, 0, 0, 200, 200);
-    SDL_RenderPresent(renderer);
+    SDL_SetWindowTitle(window, "Hello World!");
 
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(5000);
+    for (int i = 0; i < 300; i++) {
+        draw(renderer, i);
+        SDL_Delay(1000 / 60);
+    }
+
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
