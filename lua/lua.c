da2d9a8640c817d1979abf4d55c7ba91affa8f40
jaymzee <jaymzee@users.noreply.github.com>
2021-07-25 00:40:08 -0400

    sdl/lua

--- lua/lua.c
+++ lua/lua.c
@@ -1,21 +1,41 @@
 #include <lualib.h>
 #include <lauxlib.h>
 #include <SDL2/SDL.h>
-#include "draw.h"
+#include "text.h"
 #include "lua.h"
-
-const SDL_Color Yellow = {255, 255, 0, 255};
 
 extern SDL_Renderer *renderer;
 extern TTF_Font *default_font;
+
+static int clear(lua_State *L)
+{
+    unsigned int c = luaL_checknumber(L, 1);
+
+    SDL_SetRenderDrawColor(renderer, c >> 24, (c >> 16) & 0xff, (c >> 8) & 0xff, c & 0xff);
+    SDL_RenderClear(renderer);
+
+    return 0;    // number of results
+}
+
+static int pencolor(lua_State *L)
+{
+    unsigned int c = luaL_checknumber(L, 1);
+
+    SDL_SetRenderDrawColor(renderer, c >> 24, (c >> 16) & 0xff, (c >> 8) & 0xff, c & 0xff);
+
+    return 0;   // number of results
+}
 
 static int text(lua_State *L)
 {
     double x = luaL_checknumber(L, 1);
     double y = luaL_checknumber(L, 2);
     const char *s = luaL_checkstring(L, 3);
+    unsigned int c = luaL_checknumber(L, 4);
 
-    DrawText(renderer, x, y, s, default_font, Yellow);
+    SDL_Color color = {c >> 24 & 0xff, c >> 16 & 0xff, c >> 8 & 0xff, c & 0xff};
+
+    DrawText(renderer, x, y, s, default_font, color);
 
     return 0;   // number of results
 }
@@ -28,22 +48,22 @@ static int line(lua_State *L)
     double x2 = luaL_checknumber(L, 3);
     double y2 = luaL_checknumber(L, 4);
 
-    DrawLine(renderer, x1, y1, x2, y2);
+    SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
 
     return 0;   // number of results
 }
 
 static int flip(lua_State *L)
 {
-    Flip();
+    SDL_RenderPresent(renderer);
 
     return 0;   // number of results
 }
 
 static int delay(lua_State *L)
 {
-    double msec = luaL_checknumber(L, 1);
-    Delay(msec);
+    unsigned int msec = luaL_checknumber(L, 1);
+    SDL_Delay(msec);
 
     return 0;   // number of results
 }
@@ -55,6 +75,10 @@ void dofile(const char *filename)
     luaL_openlibs(L);
 
     //Expose the swap function to the lua environment
+    lua_pushcfunction(L, clear);
+    lua_setglobal(L, "clear");
+    lua_pushcfunction(L, pencolor);
+    lua_setglobal(L, "pencolor");
     lua_pushcfunction(L, text);
     lua_setglobal(L, "text");
     lua_pushcfunction(L, line);
--- lua/text.c
+++ lua/text.c
@@ -1,9 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
-extern SDL_Renderer *renderer;
-extern TTF_Font *default_font;
-
 // drawText renders a string to screen coordinates x and y in the
 // font and color given.  It is a convenience method that
 //   - creates surface
@@ -20,30 +17,4 @@ void DrawText(SDL_Renderer *renderer,
     SDL_RenderCopy(renderer, texture, NULL, &rect);
     SDL_DestroyTexture(texture);
     SDL_FreeSurface(surface);
-}
-
-void Clear(SDL_Renderer *renderer)
-{
-    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-}
-
-
-void DrawLine(SDL_Renderer *renderer,
-              double x1, double y1,
-              double x2, double y2)
-{
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 64);
-    SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
-}
-
-void Delay(unsigned int msec)
-{
-    SDL_Delay(msec);
-}
-
-void Flip(void)
-{
-    SDL_RenderPresent(renderer);
 }
