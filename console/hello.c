122c4519843af391fbbddf55a11596a2ecd00be5
jaymzee <jaymzee@users.noreply.github.com>
2021-08-05 00:33:40 +0000

    console mode demo

--- console/hello.c
+++ console/hello.c
@@ -1,43 +1,35 @@
 #include <stdio.h>
 #include "SDL.h"
 
-int main(int argc, char *args[])
+int main()
 {
-    int  i;
-    const SDL_VideoInfo *videoInfo;
-
+    const SDL_VideoInfo *videoInfo = NULL;
     SDL_Surface* screen = NULL;
-    //Start SDL
-    SDL_Init( SDL_INIT_VIDEO );
 
-    printf("video initialized, querying screen size ...\n");
+    SDL_Init(SDL_INIT_VIDEO);
+
+    printf("SDL video initialized, getting video info ...\n");
     videoInfo = SDL_GetVideoInfo();
-
     if (videoInfo) {
-        printf("got it\n");
-        int systemX = videoInfo->current_w ;
-        int systemY = videoInfo->current_h ;
-        int systemZ = videoInfo->vfmt->BitsPerPixel ;
-        printf("%d %d %d\n", systemX, systemY, systemZ);
+        int w = videoInfo->current_w ;
+        int h = videoInfo->current_h ;
+        int bits = videoInfo->vfmt->BitsPerPixel ;
+        printf("setting video mode to: %d %d %d\n", w, h, bits);
+        screen = SDL_SetVideoMode(w, h, bits, SDL_ANYFORMAT);
+    }
+    if (!screen) {
+        printf("SDL_SetVideoMode failed\n");
+        return 0;
     }
 
-    //Set up screen
-    screen = SDL_SetVideoMode(1280, 1024, 32, SDL_ANYFORMAT );
-    if (!screen)
-    {
-     	printf("SDL_SetVideoMode failed\n");
-     	return 0;
-    }
-    
-    SDL_Rect r = {0,0,320,240};
-    SDL_FillRect(screen,&r, SDL_MapRGB(screen->format, 200,200,0) );
+    SDL_ShowCursor(SDL_DISABLE);
 
-    //Update Screen
-    //SDL_Flip( screen );
-    //Pause
-    SDL_Delay( 4000 );
+    SDL_Rect r = {0, 0, 320, 240};
+    SDL_FillRect(screen, &r, SDL_MapRGB(screen->format, 200, 200, 0));
 
-    //Quit SDL
+    SDL_Flip(screen);
+    SDL_Delay(2000);
+
     SDL_Quit();
 
     return 0;
